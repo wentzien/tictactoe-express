adc8c70ea343ce5f3546a3b09db8e49ed541f6c0
Dennis Wentzien <49585347+wentzien@users.noreply.github.com>
2021-02-02 17:26:11 +0100

    parse json direct in db call

--- db/mysql.js
+++ db/mysql.js
@@ -16,12 +16,19 @@ db.getGame = (gameId) => {
     return new Promise((resolve, reject) => {
         pool.query('SELECT * FROM games WHERE gameId = ?', gameId, (err, rows, fields) => {
             if (err) return reject(err);
-            return resolve(rows);
+            if(rows[0]) {
+                let gameData = rows[0];
+                gameData.board = JSON.parse(gameData.board);
+                return resolve(gameData);
+            } else {
+                return resolve(null);
+            }
         });
     });
 };
 
 db.setGame = (game) => {
+    if(game.board) game.board = JSON.stringify(game.board);
     const fields = [
         game.gameId,
         game.aPlayerId,
@@ -42,6 +49,7 @@ db.setGame = (game) => {
 };
 
 db.updateGame = (game) => {
+    if(game.board) game.board = JSON.stringify(game.board);
     const fields = [
         game.aPlayerId,
         game.bPlayerId,
@@ -52,7 +60,6 @@ db.updateGame = (game) => {
         game.gameId
     ];
 
-    console.log(fields);
     return new Promise((resolve, reject) => {
         pool.query('UPDATE games SET aPlayerId = ?, bPlayerId = ?, board = ?, aScore = ?, bScore = ?, gameStatus = ? WHERE gameId = ?', fields,
             (err, rows, fields) => {
